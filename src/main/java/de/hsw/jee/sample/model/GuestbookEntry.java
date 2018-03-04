8ae732b1773fb3a82e2a44b8df1f399761b2a1a2
Malte Wildt <malte.wildt@flavia-it.de>
2018-03-04 11:56:42 +0100

    prepare aufgabe 1

--- src/main/java/de/hsw/jee/sample/model/GuestbookEntry.java
+++ src/main/java/de/hsw/jee/sample/model/GuestbookEntry.java
@@ -2,32 +2,16 @@ package de.hsw.jee.sample.model;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-@Entity
 public class GuestbookEntry {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
-	@Column(length=2048, nullable=false)
 	private String message;
 	
-	@Column(nullable=false)
 	private String author;
 	
-	@Temporal(TemporalType.TIMESTAMP)
 	private Date created;
 
-	@PrePersist
 	public void prePersist() {
 		this.created = new Date();
 	}
@@ -63,6 +47,5 @@ public class GuestbookEntry {
 	public void setCreated(Date created) {
 		this.created = created;
 	}
-	
 	
 }
